ce677944be5e4d2058df839705e5bdf11ba15aed
zxthecoolguy <coolgamingzx@gmail.com>
2025-05-16 18:25:24 +0000

    Update .gitignore and enhance main function for game initialization

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import pygame
 from constants import *
- 
+
 def main():
     print("Starting Asteroids!")
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:", SCREEN_HEIGHT)
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
